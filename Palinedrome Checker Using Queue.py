7dfb81e46d2d0ea7780983756fa1cd90a58dbb7b
Fc-70 <tvlkyk@gmail.com>
2025-09-12 12:07:19 +0530

    Add files via upload

---
+++ Palinedrome Checker Using Queue.py
@@ -0,0 +1,18 @@
+import collections
+
+def is_palindrome_queue(s):
+
+    char_queue = collections.deque()
+
+    for char in s:
+        if 'a' <= char.lower() <= 'z' or '0' <= char <= '9':
+            char_queue.append(char.lower())
+
+    while len(char_queue) > 1:
+        if char_queue.popleft() != char_queue.pop():
+            return "Not A Palindrome!"
+
+    return "Palindrome"
+
+name=input("Enter a String:")
+print(is_palindrome_queue(name))
